41ee94b5dabf3c13858c30f26ff6439ad97ca9f4
Efrain Flores <efradavid125@gmail.com>
2018-05-12 00:27:08 -0300

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            <div class="card ">
+                <div class="card-header bg-primary">Panel Principal</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,10 +14,12 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Bienvenido!
                 </div>
             </div>
         </div>
     </div>
+    <br>
+    
 </div>
 @endsection
